be52baee77b840b983b2824714fa0fa87cfad021
SvetlanaFedorova <64207001+SvetlanaFedorova@users.noreply.github.com>
2020-05-29 15:25:34 +0500

    Добавлено 3 файла ДЗ к 7 уроку

--- Lesson_7_task_1.py
+++ Lesson_7_task_1.py
@@ -1,15 +1,14 @@
-# Lesson 6 Task 5
+# Lesson 7 Task 1
 
 class Matrix:
     def __init__(self, a):
         my_list = []
         for i in a:
-           my_list.append([j for j in i])
+            my_list.append([j for j in i])
         self.a = my_list
 
     def __str__(self):
-        self.c = str(f'{self.a[0]},\n{self.a[1]}')
-        return self.c
+        return '\n'.join([' '.join([str(el) for el in line]) for line in self.a]) + '\n'
 
     def __add__(self, other):
         result = []
@@ -23,9 +22,13 @@ class Matrix:
                     numbers = []
         return Matrix(result)
 
-m1 = Matrix([[11, 22], [33, 44], [33, 44]])
-m2 = Matrix([[22, 33], [44, 55], [33, 44]])
-print(m1 + m2)
+m1 = Matrix([[11, 22, 33], [33, 44, 22], [33, 44, 11]])
+m2 = Matrix([[22, 33, 11], [44, 55, 33], [33, 44, 22]])
+m3 = Matrix([[22, 33, 11], [44, 55, 33], [33, 44, 22]])
+print(m1)
+print(m2)
+print(m3)
+print(m1 + m2 + m3)
 
 
 
