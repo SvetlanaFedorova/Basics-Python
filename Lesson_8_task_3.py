339a10e3921e2d12b6a3d723079dbf65ba85ebad
SvetlanaFedorova <64207001+SvetlanaFedorova@users.noreply.github.com>
2020-06-02 13:54:09 +0500

    Добавлено 4 файла ДЗ к 8 уроку

--- Lesson_8_task_3.py
+++ Lesson_8_task_3.py
@@ -1,28 +1,23 @@
 # Lesson 8 Task 3
 
-class MyOwnError(Exception):
+class OwnError(Exception):
     def __init__(self, txt):
         self.txt = txt
 
 def my_list():
-    ex = False
     res_line = []
-    while ex == False:
-        numbers = input('Введите число, или stop для выхода: ').split()
-        for num in numbers:
-            if 'stop' in num:
-                ex = True
-                break
+    while True:
+        num = input('Введите число, или stop для выхода: ')
+        if 'stop' in num:
+            break
+        try:
+            num = int(num)
+        except ValueError:
+            print('Вы ввели не число!')
+        except OwnError as err:
+            print(err)
+        else:
             res_line.append(num)
     return print(res_line)
-
-
-try:
-    if type(num) == type(int) :
-        raise MyOwnError("Вы ввели ноль, делить на ноль нельзя")
-except MyOwnError as err:
-    print(err)
-else:
-    print(f"Все хорошо. Результат деления: {1000 / inp_data}")
 
 a = my_list()
